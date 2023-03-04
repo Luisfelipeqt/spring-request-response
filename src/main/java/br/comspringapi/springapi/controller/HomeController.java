040f69f106dc88441a7d8ecc62d94fcad7c1ed1d
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-03-04 16:28:36 -0300

    Obtendo dados do cabeçalho da requisição

--- src/main/java/br/comspringapi/springapi/controller/HomeController.java
+++ src/main/java/br/comspringapi/springapi/controller/HomeController.java
@@ -1,2 +1,17 @@
-package br.comspringapi.springapi.controller;public class HomeController {
+package br.comspringapi.springapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(@RequestHeader(name = "Accept-Language", defaultValue = "Teste") String acceptLanguage) {
+        System.out.println(acceptLanguage);
+        return "home";
+    }
+
+
 }
