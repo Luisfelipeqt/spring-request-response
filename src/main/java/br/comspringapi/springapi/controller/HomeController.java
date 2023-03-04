e6ecf91d7f44a738cefa906ef9c4b1924933e16b
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-03-04 16:48:38 -0300

    Obtendo o verbo HTTP da requisição

--- src/main/java/br/comspringapi/springapi/controller/HomeController.java
+++ src/main/java/br/comspringapi/springapi/controller/HomeController.java
@@ -1,15 +1,21 @@
 package br.comspringapi.springapi.controller;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(@RequestHeader(name = "Accept-Language", defaultValue = "Teste") String acceptLanguage) {
-        System.out.println(acceptLanguage);
+
+    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+    public String home(HttpMethod method) {
+        if(method.equals(HttpMethod.GET)){
+            System.out.println("Recebi uma requisição do tipo GET");
+        } else if (method.equals(HttpMethod.POST)) {
+            System.out.println("Recebi uma requisição do tipo POST");
+        }
         return "home";
     }
 
