562ed03b778f83c585c3a9787b8c9847e264ef88
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-03-04 23:32:15 -0300

    Definindo o status da resposta

--- src/main/java/br/comspringapi/springapi/controller/HomeController.java
+++ src/main/java/br/comspringapi/springapi/controller/HomeController.java
@@ -3,6 +3,7 @@ package br.comspringapi.springapi.controller;
 import br.comspringapi.springapi.dto.ClienteDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,8 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController {
 
-    @Autowired
-    private ClienteDTO clienteDTO;
 
     @GetMapping(value = "/")
     public String home() {
@@ -21,8 +20,11 @@ public class HomeController {
 
     @PostMapping(value = "/")
     @ResponseBody
+    @ResponseStatus(code = HttpStatus.CREATED)
     public ClienteDTO homer(@RequestBody ClienteDTO clienteDTO){
-
+        System.out.println(clienteDTO.getNome());
+        System.out.println(clienteDTO.getIdade());
+        System.out.println(clienteDTO);
         return clienteDTO;
     }
 }
--- src/main/java/br/comspringapi/springapi/dto/ClienteDTO.java
+++ src/main/java/br/comspringapi/springapi/dto/ClienteDTO.java
@@ -3,12 +3,11 @@ package br.comspringapi.springapi.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Component
 public class ClienteDTO {
 
     private String nome;
