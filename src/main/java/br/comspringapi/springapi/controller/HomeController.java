ac5832c3edd941bdc08074fe89746ff693f67a4f
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-03-04 23:39:19 -0300

    Obtendo maior controle da resposta com a classe ResponseEntity

--- src/main/java/br/comspringapi/springapi/controller/HomeController.java
+++ src/main/java/br/comspringapi/springapi/controller/HomeController.java
@@ -4,11 +4,12 @@ import br.comspringapi.springapi.dto.ClienteDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@Controller // -> Se o projeto tiver Thymeleaf para mapear o Template tem que usar o @Controller em vez do @RestController
 public class HomeController {
 
 
@@ -19,12 +20,14 @@ public class HomeController {
     }
 
     @PostMapping(value = "/")
-    @ResponseBody
-    @ResponseStatus(code = HttpStatus.CREATED)
-    public ClienteDTO homer(@RequestBody ClienteDTO clienteDTO){
+    public ResponseEntity<ClienteDTO> homer(@RequestBody ClienteDTO clienteDTO){
         System.out.println(clienteDTO.getNome());
         System.out.println(clienteDTO.getIdade());
         System.out.println(clienteDTO);
-        return clienteDTO;
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Teste", "Teste")
+                .header("Teste2", "Teste2")
+                .body(clienteDTO);
     }
 }
